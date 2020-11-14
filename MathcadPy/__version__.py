22535b386064a3d7a554b4f888822d73aa91d54c
Matt Woodhead <woody.w62@gmail.com>
2020-11-14 22:36:30 +0000

    Update __version__.py

--- MathcadPy/__version__.py
+++ MathcadPy/__version__.py
@@ -9,7 +9,7 @@ Copyright 2020 Matt Woodhead
 
 __title__ = 'MathcadPy'
 __description__ = 'A Python wrapper for the Mathcad Prime COM API'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Matt Woodhead'
 __author_email__ = 'woody.w62@gmail.com'
 __license__ = 'GNU GPL v3.0'
